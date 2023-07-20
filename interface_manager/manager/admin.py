ef55dfc024ea38a501c3b6ab4fff1227dd5c5eda
Santiago.Gonzalez <santiago.gonzalezocourel@axpo.com>
2023-07-20 17:20:00 +0200

    change admin views

--- interface_manager/manager/admin.py
+++ interface_manager/manager/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from .models import Interface, System, Endpoint, BusinessProcess
+from .models import Interface, System, BusinessProcess
+
+
 
 class SystemAdmin(admin.ModelAdmin):
-
+    fields = ["name", "responsible", "technology", "is_monitoring", "monitoring URL", "is_alarmed", "is_external", "production_ready", "deprecation_date"]
     list_display = ["name", "responsible", "technology", "production_ready"]
 
 class InterfaceAdmin(admin.ModelAdmin):
     list_display = ["get_business_process_dpto", "get_business_process_name", "get_source_name", "get_destination_name"]
+
+
 
     def get_business_process_dpto(self, obj):
         return obj.business_process.get_department_display()
@@ -21,16 +25,12 @@ class InterfaceAdmin(admin.ModelAdmin):
     def get_destination_name(self, obj):
         return obj.destination.name
 
-class EndpointAdmin(admin.ModelAdmin):
-    list_display = ["name", "description", "url"]
-
 class BusinessProcessAdmin(admin.ModelAdmin):
     list_display = ["department", "name"]
 
 
 admin.site.register(System, SystemAdmin)
 admin.site.register(Interface, InterfaceAdmin)
-admin.site.register(Endpoint, EndpointAdmin)
 admin.site.register(BusinessProcess, BusinessProcessAdmin)
 
 
